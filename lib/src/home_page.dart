2b82eb64145dcf56d433b2064bc7601fcc08880a
iran21 <iransneto2@gmail.com>
2020-12-21 00:13:34 -0300

    #aula10 - A "cara" do Aplicativo (Scaffold)

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -12,16 +12,32 @@ class HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutterando'),
+        centerTitle: true,
+      ),
+      body: Center(
         child: GestureDetector(
-            onTap: () {
-              print('clicando');
-              setState(() {
-                count++;
-              });
-            },
-            child: Text('clicando $count')),
+          onTap: () {
+            print('clicando');
+            setState(() {
+              count++;
+            });
+          },
+          child: Text(
+            'clicando $count',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
